d1f442e26a02cc0667ded46afb5516fbbcb4e5bf
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2020-10-28 11:29:08 +0000

    Ver 3.9.1 updates

---
+++ interface/js/airlink.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    $.ajax({url: "api/settings/version.json", dataType:"json", success: function (result) {
+        $('#Version').text(result.Version);
+        $('#Build').text(result.Build);
+    }});
+
+    var countsOutTable = $('#CountsOutTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkCountsOut.json'
+    });
+
+    var aqiOutTable = $('#AqiOutTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkAqiOut.json'
+    });
+
+    var pctOutTable = $('#PctOutTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkPctOut.json'
+    });
+
+    var countsInTable = $('#CountsInTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkCountsIn.json'
+    });
+
+    var aqiInTable = $('#AqiInTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkAqiIn.json'
+    });
+
+    var pctInTable = $('#PctInTable').DataTable({
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ordering": false,
+        "ajax": '../api/extra/airLinkPctIn.json'
+    });
+
+
+    setInterval(function () {
+        countsOutTable.ajax.url('../api/extra/airLinkCountsOut.json').load();
+        aqiOutTable.ajax.url('../api/extra/airLinkAqiOut.json').load();
+        pctOutTable.ajax.url('../api/extra/airLinkPctOut.json').load();
+        countsInTable.ajax.url('../api/extra/airLinkCountsIn.json').load();
+        aqiInTable.ajax.url('../api/extra/airLinkAqiIn.json').load();
+        pctInTable.ajax.url('../api/extra/airLinkPctIn.json').load();
+    }, 10000);
+
+});
+
+
