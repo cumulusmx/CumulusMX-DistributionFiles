1f5df141041d8fb625a6711629edf066de575e14
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2021-02-20 20:49:06 +0000

    b3113 - fix to gauges popups

--- webfiles/js/setpagedata.js
+++ webfiles/js/setpagedata.js
@@ -1,6 +1,6 @@
 /*	----------------------------------------------------------
  * 	at-basic.js		v:0.0.2		d:Feb 2021		a:Neil  Thomas
- *  Last modified: 2021/02/19 13:18:13
+ *  Last modified: 2021/02/19 23:20:06
  * 	Basic scripts for all new at-xxxx.html template pages.
  *  Combined with existing MX setpagedata.js
  * 	Requires jQuery
@@ -17,12 +17,49 @@ let footerMargin = 10;	//	Gap between the body and the footer
 
 //	Thats the only changes you should make unless you know better!
 
+let menu = '<a href="index.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Now</a>' +
+	'<a href="gauges.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Gauges</a>' +
+	'<a href="today.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Today</a>' +
+	'<a href="yesterday.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Yesterday</a>' +
+	'<a href="thismonth.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">This Month</a>' +
+	'<a href="thisyear.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">This Year</a>' +
+	'<a href="record.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Records</a>' +
+	'<a href="monthlyrecord.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Monthly Records</a>' +
+	'<div class="w3-dropdown-hover">' +
+		'<button class="w3-btn w3-theme-hvr  at-slim w3-hide-medium w3-hide-small">Charts</button>' +
+		'<div class="w3-dropdown-content w3-bar-block w3-theme">' +
+			'<a href="trends.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Trends</a>' +
+			'<a href="selectachart.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Select-a-graph</a>' +
+			'<a href="historic.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Historic</a>' +
+		'</div>' +
+	'</div>' +
+	'<a href="https://cumulus.hosiene.co.uk/index.php" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium w3-hide forumlink" target="_blank">Forum</a>' +
+	'<a href="#" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium w3-hide webcamlink">Webcam</a>' +
+	'<button class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-large w3-right" onClick="toggleMenu(\'Main_Menu_Mobile\')">&#9776;</button>';
+
+let mobileMenu = '<a href="index.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Now</a>' +
+	'<a href="gauges.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Gauges</a>' +
+	'<a href="today.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Today</a>' +
+	'<a href="yesterday.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Yesterday</a>' +
+	'<a href="thismonth.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">This Month</a>' +
+	'<a href="thisyear.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">This Year</a>' +
+	'<a href="record.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Records</a>' +
+	'<a href="monthlyrecord.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Monthly Records</a>' +
+	'<a href="trends.html" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Trends</a>' +
+	'<a href="selectachart.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Select-a-graph</a>' +
+	'<a href="historic.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Historic</a>' +
+	'<a href="https://cumulus.hosiene.co.uk/index.php" class="w3-bar-item w3-btn w3-theme-hvr w3-hide at-slim forumlink" target="_blank">Forum</a>' +
+	'<a href="#" class="w3-bar-item w3-btn w3-theme-hvr w3-hide at slim webcamlink">Webcam</a>';
+
+let cmx_data;
+
 $(document).ready( function() {
 	setupPage();
+	getAvail();
 	getPageData();
 });
 
-var setupPage = function() {
+let setupPage = function() {
 	// menus
 	$('#Main_Menu').html(menu);
 	$('#Main_Menu_Mobile').html(mobileMenu);
@@ -53,13 +90,13 @@ var setupPage = function() {
 	}
 };
 
-var toggleMenu = function(menuid) {
+let toggleMenu = function(menuid) {
 	$('#'+menuid).toggleClass('w3-show');
 };
 
 
 // Get the main page data
-var getPageData = function () {
+let getPageData = function () {
 	$.getJSON('websitedata.json', function (json) {
 		console.log('Data success');
 		cmx_data = json;
@@ -94,56 +131,19 @@ var getPageData = function () {
 		$('#cmx-location').trigger('change');
 	})
 	.fail(function (jqxhr, textStatus, error) {
-		var err = textStatus + ', ' + error;
+		let err = textStatus + ', ' + error;
 		console.log('Data Request Failed: ' + err );
 	});
 };
 
 // get the graph config file
-$.getJSON('availabledata.json', function (json) {
-	if (undefined !== json.Solar && json.Solar.length > 0 && json.Solar[0] === 'Solar Rad') {
-		$('.solar-data').removeClass('w3-hide');
-	}
-	if (undefined !== json.Solar && json.Solar.length > 0 && (json.Solar[0] === 'UV Index' || json.Solar[1] === 'UV Index')) {
-		$('.uv-data').removeClass('w3-hide');
-	}
-});
-
-
-
-var cmx_data;
-
-var menu = '<a href="index.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Now</a>' +
-	'<a href="gauges.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Gauges</a>' +
-	'<a href="today.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Today</a>' +
-	'<a href="yesterday.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Yesterday</a>' +
-	'<a href="thismonth.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">This Month</a>' +
-	'<a href="thisyear.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">This Year</a>' +
-	'<a href="record.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Records</a>' +
-	'<a href="monthlyrecord.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium">Monthly Records</a>' +
-	'<div class="w3-dropdown-hover">' +
-		'<button class="w3-btn w3-theme-hvr  at-slim w3-hide-medium w3-hide-small">Charts</button>' +
-		'<div class="w3-dropdown-content w3-bar-block w3-theme">' +
-			'<a href="trends.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Trends</a>' +
-			'<a href="selectachart.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Select-a-graph</a>' +
-			'<a href="historic.htm" class="w3-bar-item w3-btn w3-theme-d5-hvr at-slim">Historic</a>' +
-		'</div>' +
-	'</div>' +
-	'<a href="https://cumulus.hosiene.co.uk/index.php" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium w3-hide forumlink" target="_blank">Forum</a>' +
-	'<a href="#" class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-small w3-hide-medium w3-hide webcamlink">Webcam</a>' +
-	'<button class="w3-bar-item w3-btn w3-theme-hvr at-slim w3-hide-large w3-right" onClick="toggleMenu(\'Main_Menu_Mobile\')">&#9776;</button>';
-
-var mobileMenu = '<a href="index.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Now</a>' +
-	'<a href="gauges.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Gauges</a>' +
-	'<a href="today.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Today</a>' +
-	'<a href="yesterday.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Yesterday</a>' +
-	'<a href="thismonth.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">This Month</a>' +
-	'<a href="thisyear.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">This Year</a>' +
-	'<a href="record.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Records</a>' +
-	'<a href="monthlyrecord.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Monthly Records</a>' +
-	'<a href="trends.html" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Trends</a>' +
-	'<a href="selectachart.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Select-a-graph</a>' +
-	'<a href="historic.htm" class="w3-bar-item w3-btn w3-theme-hvr at-slim">Historic</a>' +
-	'<a href="https://cumulus.hosiene.co.uk/index.php" class="w3-bar-item w3-btn w3-theme-hvr w3-hide at-slim forumlink" target="_blank">Forum</a>' +
-	'<a href="#" class="w3-bar-item w3-btn w3-theme-hvr w3-hide at slim webcamlink">Webcam</a>';
-
+let getAvail = function () {
+	$.getJSON('availabledata.json', function (json) {
+		if (undefined !== json.Solar && json.Solar.length > 0 && json.Solar[0] === 'Solar Rad') {
+			$('.solar-data').removeClass('w3-hide');
+		}
+		if (undefined !== json.Solar && json.Solar.length > 0 && (json.Solar[0] === 'UV Index' || json.Solar[1] === 'UV Index')) {
+			$('.uv-data').removeClass('w3-hide');
+		}
+	});
+}
