61f55d75ea597d55c626f845aa8cb5b0628a6dce
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2021-03-16 08:57:35 +0000

    v3.10.3 support

--- webfiles-legacy/js/setpagedata.js
+++ webfiles-legacy/js/setpagedata.js
@@ -1,5 +1,5 @@
 // Created: 2021/02/09 10:51:54
-// Last modified: 2021/02/20 23:29:27
+// Last modified: 2021/03/04 15:28:18
 
 var cmx_data;
 
@@ -7,17 +7,17 @@ $.getJSON("websitedata.json", function (json) {
     console.log( "Data success" );
     cmx_data = json;
 
-    $(document).prop('title', cmx_data['cmx-location'] + ' weather');
-    $('meta[name=description]').attr('content', cmx_data['cmx-location'] + ' weather data');
-    $('meta[name=keywords]').attr('content', $('meta[name=keywords]').attr('content') + ', ' + cmx_data['cmx-location'] + ' weather data');
+    $(document).prop('title', cmx_data['location'] + ' weather');
+    $('meta[name=description]').attr('content', cmx_data['location'] + ' weather data');
+    $('meta[name=keywords]').attr('content', $('meta[name=keywords]').attr('content') + ', ' + cmx_data['location'] + ' weather data');
 
     // Update all spans with id beginning "cmx-"
     $('span[id^=cmx-]').each(function () {
-        this.innerHTML = cmx_data[this.id];
+        this.innerHTML = cmx_data[this.id.split('-')[1]];
     });
     // Update all spans with class beginning "cmx-"
     $('span[class^=cmx-]').each(function () {
-        this.innerHTML = cmx_data[this.className];
+        this.innerHTML = cmx_data[this.className.split('-')[1]];
     });
 
     // Use this to trigger other scripts on the page
