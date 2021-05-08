f748fd1f94e8fcb5026039ff9a1e3ff66c3e9094
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2021-05-08 23:42:02 +0100

    v3.11.1 support

--- interface/js/programsettings.js
+++ interface/js/programsettings.js
@@ -1,4 +1,6 @@
-// Last modified: 2021/04/26 15:01:01
+// Last modified: 2021/05/08 17:45:19
+
+let accessMode;
 
 $(document).ready(function () {
 
@@ -9,6 +11,18 @@ $(document).ready(function () {
         //"view": "bootstrap-edit",
         "ui": "bootstrap",
         "postRender": function (form) {
+            // Change in accessibility is enabled
+            let accessObj = form.childrenByPropertyId["accessible"];
+            onAccessChange(null, accessObj.getValue());
+            accessMode = accessObj.getValue();
+
+            if (!accessMode) {
+                setCollapsed();  // sets the class and aria attribute missing on first load by Alpaca
+            }
+
+            // Trigger changes is the accessibility mode is changed
+            accessObj.on("change", function() {onAccessChange(this)});
+
             $("#save-button").click(function () {
                 if (form.isValid(true)) {
                     var json = form.getValue();
@@ -30,3 +44,83 @@ $(document).ready(function () {
         }
     });
 });
+
+
+function addButtons() {
+    $('#form legend').each(function () {
+        let span = $('span:first',this);
+        if (span.length === 0)
+            return;
+
+        let butt = $('<button type="button" data-toggle="collapse" data-target="' +
+            $(span).attr('data-target') +
+            '" role="treeitem" aria-expanded="false" class="collapsed">' +
+            $(span).text() +
+            '</button>');
+        $(span).remove();
+        $(this).prepend(butt);
+    });
+}
+
+function removeButtons() {
+    $('#form legend').each(function () {
+        let butt = $('button:first',this);
+        if (butt.length === 0)
+            return;
+
+        let span = $('<span data-toggle="collapse" data-target="' +
+            $(butt).attr('data-target') +
+            '" role="treeitem" aria-expanded="false" class="collapsed">' +
+            $(butt).text() +
+            '</span>');
+        $(butt).remove();
+        $(this).prepend(span);
+    });
+}
+
+function setCollapsed() {
+    $('#form div.alpaca-container.collapse').each(function () {
+        let span = $(this).siblings('legend:first').children('span:first');
+        if ($(this).hasClass('in')) {
+            span.attr('role', 'treeitem');
+            span.attr('aria-expanded', true);
+        } else {
+            span.attr('role', 'treeitem');
+            span.attr('aria-expanded', false);
+            span.addClass('collapsed')
+        }
+    });
+}
+
+function getCSSRule(search) {
+    for (let x = 0; x < document.styleSheets.length; x++) {
+        let rules = document.styleSheets[x].rules || document.styleSheets[x].cssRules;
+        for (let i = 0; i < rules.length; i++) {
+            if (rules[i].selectorText && rules[i].selectorText.lastIndexOf(search) === 0  && search.length === rules[i].selectorText.length) {
+                return rules[i];
+            }
+        }
+    }
+    return null;
+}
+
+function onAccessChange(that, val) {
+    let mode = val == null ? that.getValue() : val;
+    if (mode == accessMode) {
+        return;
+    }
+
+    let expandable = getCSSRule('.alpaca-field > legend > .collapsed::before');
+    let expanded = getCSSRule('.alpaca-field > legend > span::before');
+
+    accessMode = mode;
+    if (mode) {
+        expandable.style.setProperty('display','none');
+        expanded.style.setProperty('display','none');
+        addButtons();
+    } else {
+        expandable.style.removeProperty('display');
+        expanded.style.removeProperty('display');
+        removeButtons();
+    }
+}
