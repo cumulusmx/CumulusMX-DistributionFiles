e8aaa93b1b24d21eace67c170df927fa2c2750cd
OaktreeWeather <neil.thomas@oaktreewebs.co.uk>
2025-12-18 17:46:57 +0000

    Updated build number

--- interface/ai2/js/ai-page-ctrl.js
+++ interface/ai2/js/ai-page-ctrl.js
@@ -8,7 +8,7 @@
 
 let cmxConfig = {};
 let cmxSession = {};
-let build = 0.01;
+let build = 1.0;
 
 //  Construct variable to access Local, and Session storage 
 var url=document.baseURI.split('/');
