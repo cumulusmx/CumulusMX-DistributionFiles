3e7aad75a60e257cf92d30a09d23df848f26199a
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2026-07-21 23:03:44 +0100

    Add midnight rain to Todays Rain Editor

--- interface/js/raintodayeditor.js
+++ interface/js/raintodayeditor.js
@@ -1,12 +1,14 @@
-// Last modified: 2024/10/29 11:21:33
+// Last modified: 2026/07/21 22:30:21
 
 $(document).ready(function() {
 
     $.ajax({url: '/api/edit/raintodayeditdata.json', dataType:'json', success: function (result) {
         $('#inputCurrentCounter').val(result.raincounter);
         $('#inputStartCounter').val(result.startofdayrain);
+        $('#inputMidnightCounter').val(result.midnightcounter);
         $('#inputRainMultiplier').val(result.rainmult);
         $('#inputRainToday').val(result.raintoday);
+        $('#inputMidnightRain').val(result.midnightrain);
 
         $('#inputRainToday').attr('step',result.step);
     }});
