26e6b1620e999fd123c96e0dc75a618dcc901a16
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2026-09-23 12:12:14 +0100

    Extra Web Files updates

--- interface/js/extrawebfiles.js
+++ interface/js/extrawebfiles.js
@@ -1,160 +1,153 @@
-/*
- * examples/full/javascript/demo.js
- *
- * Last modified: 2025/08/15 22:48:21
- *
- * This file is part of EditableGrid.
- * http://editablegrid.net
- *
- * Copyright (c) 2011 Webismymind SPRL
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://editablegrid.net/license
- */
+// Last modified: 2026/09/22 18:53:30
 
-// create our editable grid
-var editableGrid = new EditableGrid('DemoGridFull', {
-	enableSort: false, // true is the default, set it to false if you don't want sorting to be enabled
-	pageSize: 15
-});
-
-// helper function to get path of a demo image
-function image(relativePath) {
-	return 'js/images/' + relativePath;
-}
+let accessMode;
+let csvChar;
 
 
-// register the function that will handle model changes
-editableGrid.modelChanged = function(rowIndex, columnIndex, oldValue, newValue) {
+$(document).ready(function () {
+    $.ajax({url: '/api/info/version.json', dataType: 'json', success: function (result) {
+        $('#Version').text(result.Version);
+        $('#Build').text(result.Build);
+    }});
 
-    $.ajax({
-        url: '/api/setsettings/updateextrawebfiles.html',
-        type: 'POST',
-        dataType: 'text',
-        data: {
-            id: editableGrid.getRowId(rowIndex),
-            column: columnIndex,
-            value: newValue
+    $('#filesForm').alpaca({
+        dataSource: '/api/settings/extrawebfiles.json',
+        optionsSource: '/json/ExtraWebFilesOptions.json',
+        schemaSource: '/json/ExtraWebFilesSchema.json',
+        ui: 'bootstrap',
+        view: 'bootstrap-edit',
+        options: {
+            form: {
+                buttons: {
+                    // don't use the Submit button because that is disabled on validation errors
+                    validate: {
+                        title: '{{SAVE_SETTINGS}}',
+                        click: function() {
+                            this.refreshValidationState(true);
+                            if (this.isValid(true)) {
+                                let json = this.getValue();
+
+                                $.ajax({
+                                    type: 'POST',
+                                    url: '/api/setsettings/updateextrawebfiles.json',
+                                    data: {json: JSON.stringify(json)},
+                                    dataType: 'text'
+                                })
+                                .done(function () {
+                                    alert('{{SETTINGS_UPDATED}}');
+                                })
+                                .fail(function (jqXHR, textStatus) {
+                                    alert('Error: ' + jqXHR.status + '(' + textStatus + ') - ' + jqXHR.responseText);
+                                });
+                            } else {
+                                let firstErr = $('form').find('.has-error:first')
+                                let path = $(firstErr).attr('data-alpaca-field-path');
+                                let msg = $(firstErr).children('.alpaca-message').text();
+                                alert('{{INVALID_VALUE_IN_FORM}}: ' + path + msg);
+                                if ($(firstErr).is(':visible')) {
+                                    let entry = $(firstErr).focus();
+                                    $(window).scrollTop($(entry).position().top);
+                                }
+                            }
+                        },
+                        styles: 'alpaca-form-button-submit'
+                    }
+                }
+            }
         },
+        postRender: function (form) {
+            // Change in accessibility is enabled
+            let accessObj = form.childrenByPropertyId['Accessible'];
+            onAccessChange(null, accessObj.getValue());
+            accessMode = accessObj.getValue();
 
-        success: function (response) {
-            // reset old value if failed
-            if (response != 'success') editableGrid.setValueAt(rowIndex, columnIndex, oldValue);
-            // here you could also highlight the updated row to give the user some feedback
-        },
+            if (!accessMode) {
+                setCollapsed();  // sets the class and aria attribute missing on first load by Alpaca
+            }
 
-        error: function(XMLHttpRequest, textStatus, exception) {
-            alert(XMLHttpRequest.responseText);
+            // Trigger changes is the accessibility mode is changed
+            //accessObj.on('change', function() {onAccessChange(this)});
         }
     });
-};
+});
 
+function addButtons() {
+    $('form legend').each(function () {
+        let span = $('span:first',this);
+        if (span.length === 0)
+            return;
 
-// Override the checkbox renderer to make it accessible
-CheckboxCellRenderer.prototype.render = function(element, value)
-{
-	// convert value to boolean just in case
-	value = (value && value != 0 && value != 'false') ? true : false;
+        let butt = $('<button type="button" data-toggle="collapse" data-target="' +
+            $(span).attr('data-target') +
+            '" role="treeitem" aria-expanded="false" class="collapsed">' +
+            $(span).text() +
+            '</button>');
+        $(span).remove();
+        $(this).prepend(butt);
+    });
+}
 
-	// if check box already created, just update its state
-	if (element.firstChild) { element.firstChild.checked = value; return; }
+function removeButtons() {
+    $('form legend').each(function () {
+        let butt = $('button:first',this);
+        if (butt.length === 0)
+            return;
 
-	// create and initialize checkbox
-	var htmlInput = document.createElement('input');
-	htmlInput.setAttribute('type', 'checkbox');
-	htmlInput.setAttribute('id', this.column.name + '-' + element.rowIndex)
-	var htmlLabel = document.createElement('label');
-	htmlLabel.setAttribute('for', this.column.name + '-' + element.rowIndex)
-	htmlLabel.setAttribute('style', 'position:absolute; top:-1000px');
-	htmlLabel.innerHTML = this.column.label;
+        let span = $('<span data-toggle="collapse" data-target="' +
+            $(butt).attr('data-target') +
+            '" role="treeitem" aria-expanded="false" class="collapsed">' +
+            $(butt).text() +
+            '</span>');
+        $(butt).remove();
+        $(this).prepend(span);
+    });
+}
 
-	// give access to the cell editor and element from the editor field
-	htmlInput.element = element;
-	htmlInput.cellrenderer = this;
+function setCollapsed() {
+    $('form div.alpaca-container.collapse').each(function () {
+        let span = $(this).siblings('legend:first').children('span:first');
+        if ($(this).hasClass('in')) {
+            span.attr('role', 'treeitem');
+            span.attr('aria-expanded', true);
+        } else {
+            span.attr('role', 'treeitem');
+            span.attr('aria-expanded', false);
+            span.addClass('collapsed')
+        }
+    });
+}
 
-	// this renderer is a little special because it allows direct edition
-	var cellEditor = new CellEditor();
-	cellEditor.editablegrid = this.editablegrid;
-	cellEditor.column = this.column;
-	htmlInput.onclick = function(event) {
-		element.rowIndex = this.cellrenderer.editablegrid.getRowIndex(element.parentNode); // in case it has changed due to sorting or remove
-		element.isEditing = true;
-		cellEditor.applyEditing(element, htmlInput.checked ? true : false);
-	};
+function getCSSRule(search) {
+    for (let sheet of document.styleSheets) {
+        if (sheet.href.includes('alpaca')) {
+            const rules = sheet.cssRules || sheet.rules;
+            for (let rule of rules) {
+                if (rule.selectorText && rule.selectorText.lastIndexOf(search) === 0) {
+                    return rule;
+                }
+            }
+        }
+    }
+    return null;
+}
 
-	element.appendChild(htmlInput);
-	element.appendChild(htmlLabel);
-	htmlInput.checked = value;
-	htmlInput.disabled = (!this.column.editable || !this.editablegrid.isEditable(element.rowIndex, element.columnIndex));
+function onAccessChange(that, val) {
+    let mode = val == null ? that.getValue() : val;
+    if (mode == accessMode) {
+        return;
+    }
 
-	EditableGrid.prototype.addClassName(element, 'boolean');
-};
+    let expandable = getCSSRule('.alpaca-field > legend > .collapsed::before');
+    let expanded = getCSSRule('.alpaca-field > legend > span::before');
 
-
-// this function will initialize our editable grid
-EditableGrid.prototype.initializeGrid = function()
-{
-	with (this) {
-		// update paginator whenever the table is rendered (after a sort, filter, page change, etc.)
-		tableRendered = function() { this.updatePaginator(); };
-
-		// render the grid (parameters will be ignored if we have attached to an existing HTML table)
-		renderGrid('tablecontent', 'testgrid', 'tableid');
-	}
-};
-
-
-EditableGrid.prototype.onloadJSON = function(url)
-{
-	// register the function that will be called when the XML has been fully loaded
-	this.tableLoaded = function() {
-		this.initializeGrid();
-	};
-
-	// load JSON URL
-	this.loadJSON(url);
-};
-
-
-// function to render the paginator control
-EditableGrid.prototype.updatePaginator = function()
-{
-	var paginator = $('#paginator').empty();
-	var nbPages = this.getPageCount();
-
-	// get interval
-	var interval = this.getSlidingPageInterval(15);
-	if (interval == null) return;
-
-	// get pages in interval (with links except for the current page)
-	var pages = this.getPagesInInterval(interval, function(pageIndex, isCurrent) {
-		if (isCurrent) return '' + (pageIndex + 1);
-		return $('<a>').css('cursor', 'pointer').html(pageIndex + 1).click(function(event) { editableGrid.setPageIndex(parseInt($(this).html()) - 1); });
-	});
-
-	// "first" link
-	var link = $('<a>').html('<img src="' + image('gofirst.png') + '"/>&nbsp;');
-	if (!this.canGoBack()) link.css({ opacity : 0.4, filter: 'alpha(opacity=40)' });
-	else link.css('cursor', 'pointer').click(function(event) { editableGrid.firstPage(); });
-	paginator.append(link);
-
-	// "prev" link
-	link = $('<a>').html('<img src="' + image('prev.png') + '"/>&nbsp;');
-	if (!this.canGoBack()) link.css({ opacity : 0.4, filter: 'alpha(opacity=40)' });
-	else link.css('cursor', 'pointer').click(function(event) { editableGrid.prevPage(); });
-	paginator.append(link);
-
-	// pages
-	for (p = 0; p < pages.length; p++) paginator.append(pages[p]).append(' | ');
-
-	// "next" link
-	link = $('<a>').html('<img src="' + image('next.png') + '"/>&nbsp;');
-	if (!this.canGoForward()) link.css({ opacity : 0.4, filter: 'alpha(opacity=40)' });
-	else link.css('cursor', 'pointer').click(function(event) { editableGrid.nextPage(); });
-	paginator.append(link);
-
-	// "last" link
-	link = $('<a>').html('<img src="' + image('golast.png') + '"/>&nbsp;');
-	if (!this.canGoForward()) link.css({ opacity : 0.4, filter: 'alpha(opacity=40)' });
-	else link.css('cursor', 'pointer').click(function(event) { editableGrid.lastPage(); });
-	paginator.append(link);
-};
+    accessMode = mode;
+    if (mode) {
+        expandable.style.setProperty('display','none');
+        expanded.style.setProperty('display','none');
+        addButtons();
+    } else {
+        expandable.style.setProperty('display','');
+        expanded.style.setProperty('display','');
+        removeButtons();
+    }
+}
